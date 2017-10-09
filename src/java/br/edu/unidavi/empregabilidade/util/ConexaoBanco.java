a021e752aee76b041f747109a5a650a79c610f49
Fábio Zoz <zozfabio@gmail.com>
2017-10-09 14:20:33 -0300

    Correção da classe de conexão.

--- src/java/br/edu/unidavi/empregabilidade/util/ConexaoBanco.java
+++ src/java/br/edu/unidavi/empregabilidade/util/ConexaoBanco.java
@@ -5,104 +5,89 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConexaoBanco {
     
+    private static final Logger LOG = Logger.getLogger(ConexaoBanco.class.getName());
+
+    private static final String USUARIO = "root";
+    private static final String SENHA = "pu1{n-ENTPoV=Zu";
+    private static final String BANCO = "root";
+    private static final String HOST = "localhost";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://"+HOST+":3306/"+BANCO;
+
+    private static boolean conectado = false;
+    private static Connection con = null;
+    private static ConexaoBanco connection = null;
+    
     private Statement stmt;
     private ResultSet rs;
-    private static Connection con;  
-    private static boolean conectado;
-    private static String usuario = "marcondes";
-    private static String senha = "socdconde";
-    private static String banco = "empregos";
-    private static String host = "localhost";
-    private static String driver = "com.mysql.jdbc.Driver";
-    private String user;
-    private static String url = "jdbc:mysql://"+host+":3307/"+banco;
-    private static ConexaoBanco conect = null;
-    
-    public ConexaoBanco(String Url, String Usuario, String Senha) {
-        conectado = false;
-        url = Url;
-        usuario = Usuario;
-        senha = Senha;
-        user = null;
-    }
     
     private static void conectar() {
-        Connection conexao = null;
-        try{
-            Class.forName(driver);
-            conexao = DriverManager.getConnection(url, usuario, senha);            
-        }
-        catch(ClassNotFoundException erro){
-            System.out.println("Erro de driver!");
-        }
-        catch(SQLException erro){
-            System.out.println("Erro de conexão!");
-        }
-        finally{
-            con = conexao;
-        }
-    }
-    
-    public ResultSet getDados(String comando) throws Exception{       
-        rs = null;
-            if(!conectado)
-                conectar();
-
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery( comando );
-        return rs;
-    }
-    
-    public boolean setDados(String comando) {
-        if(!conectado)
-            conectar();
         try {
-            Statement stmt = con.createStatement();
-            stmt.executeUpdate( comando );
-            return true;
-        } 
-        catch (Exception e) {
-            e.printStackTrace();
-            return false;
+            Class.forName(DRIVER);
+            con = DriverManager.getConnection(URL, USUARIO, SENHA);
+            connection = new ConexaoBanco();
+            conectado = true;
+        } catch(ClassNotFoundException ex){
+            LOG.log(Level.SEVERE, "Erro de driver!", ex);
+        } catch(SQLException ex){
+            LOG.log(Level.SEVERE, "Erro de conexão!", ex);
         }
     }
     
-    /**
-     *Retorna a conexao com o banco de dados
-     */
     public static Connection getConexao() {
-       if(!conectado) {
+       if (!conectado) {
            conectar();
        }
        return con;
     }
-    
+
     public static ConexaoBanco getConect() {
-        if(conect == null){
+        if (connection == null){
             conectarPrimeiraVez();
         }
-        return conect;
+        return connection;
     }
-    
+
     public static void conectarPrimeiraVez() {
-        conect = new ConexaoBanco("jdbc:mysql://localhost:3307/empregos", "marcondes", "socdconde");
-        conect.conectar();
+        ConexaoBanco.conectar();
     }
     
-            
+    public ResultSet getDados(String comando) throws Exception {
+        if (!conectado) {
+            conectar();
+        }
+        stmt = con.createStatement();
+        rs = stmt.executeQuery(comando);
+        return rs;
+    }
+    
+    public boolean setDados(String comando) {
+        if (!conectado) {
+            conectar();
+        }
+        try {
+            stmt = con.createStatement();
+            stmt.executeUpdate(comando);
+            return true;
+        } catch (SQLException ex) {
+            LOG.log(Level.SEVERE, "Erro de SQL!", ex);
+            return false;
+        }
+    }
+
     protected void sairTela() {
-        if(conectado) {
+        if (conectado) {
             try {
                 stmt.close();
                 con.close();
-            }
-            catch(Exception e) {
-                e.printStackTrace();
+            } catch(SQLException ex) {
+                LOG.log(Level.SEVERE, "Erro de SQL!", ex);
             }
         }
     }
-    
 }
